b069293ffd0c047fe78ee1a8ead83c111e6f1e76
Andrés Liu <adavidliuf@gmail.com>
2018-11-02 21:59:38 -0500

    Resolución de conflictos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-//@prepros-prepend "./modules/header.js";
-
-=======
->>>>>>> origin/master
 $(document).ready(function () {
     
     $("body").bootstrapMaterialDesign();
     
-<<<<<<< HEAD
-    const header = new Header();
-    
-=======
->>>>>>> origin/master
 });
